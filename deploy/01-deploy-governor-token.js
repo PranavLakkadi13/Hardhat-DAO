c1ca9a8ed2f21feff761b854e36a85602cea5680
Pranav <pranavlakkadi@gmail.com>
2023-09-21 00:48:37 +0530

    third commit

--- deploy/01-deploy-governor-token.js
+++ deploy/01-deploy-governor-token.js
@@ -1,21 +1,33 @@
-const {ethers, network } = require("hardhat");
+const {ethers } = require("hardhat");
 
 module.exports = async ({getNamedAccounts,deployments}) => {
     const {deploy,log} = deployments;
     const {deployer} = await getNamedAccounts();
-    const chainId = network.config.chainId;
-
-    args = [];
 
     log("Deploying the governance token contract.....");
     const governerToken = await deploy("GovernanceToken", {
         from : deployer,
         log: true,
-        args: args,
+        args: [],
         waitConfirmations: network.config.blockConfirmations || 1,
     })
 
     log("The Governance token has been deployed........");
     log(`The token address is ${governerToken.address}`);
-    log("----------------------------------------------------------------")
+    log("----------------------------------------------------------------");
+
+    log("----------------------------------------------------------------");
+    await delegate(governerToken.address, deployer);
+    log("delegated.....")
+    log("----------------------------------------------------------------");
 };
+
+
+const delegate = async (tokenaddress, delegatedTo) => {
+    const governanceToken = await ethers.getContractAt("GovernanceToken",tokenaddress);
+
+    const tx = await governanceToken.delegate(delegatedTo);
+    await tx.wait(1);
+    console.log(`checkpoints ${await governanceToken.numCheckpoints(delegatedTo)}`);
+    
+}
---
+++ deploy/02-deploy-timelock.js
@@ -0,0 +1,22 @@
+const {ethers} = require("hardhat");
+const {min_delay} = require("../helper-hardhat-config");
+
+module.exports = async ({getNamedAccounts, deployments}) => {
+    const {deploy,log} = deployments;
+    const {deployer} = await getNamedAccounts();
+
+    log("Deploying the time-lock contract ");
+
+     const args = [min_delay, [], [], deployer];
+
+    const TimeLock = await deploy("TimeLock", {
+        from: deployer,
+        log: true,
+        args: args,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
+
+    log("The Governance token has been deployed........");
+    log(`The token address is ${TimeLock.address}`);
+    log("----------------------------------------------------------------");
+}
